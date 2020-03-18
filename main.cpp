f72070807095caf71f83215ae7416a81fa56cb62
leolcx <61586931+leolcx@users.noreply.github.com>
2020-03-18 16:29:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
-#include "Fonctions/Tableaux.cpp"
+#include "fonctions.cpp"
 #define EVAL(exp) cout << "Valeur de " << (#exp) <<" : "<< (exp) << endl
 
 
 int main()
 {
     printTab();
-    EVAL(autantDe01SurColones());
-    EVAL(autantDe01SurLignes());
     max2cac();
     return 0;
 }
