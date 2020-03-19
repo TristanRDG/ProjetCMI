f7bf59e29d23ab65905313812d63a2c567a68209
leolcx <61586931+leolcx@users.noreply.github.com>
2020-03-19 16:18:27 +0100

    Create printTab.cpp

---
+++ Fonctions Détails/printTab.cpp
@@ -0,0 +1,12 @@
+void printTab () {
+    
+    for (int i = 0; i <= 3; i++)
+    {
+        cout<<"\n";
+        for (int j = 0; j <=3; j++)
+        {
+            cout<<tableauTest[i][j]<<" ";
+        }
+    }
+    cout<<endl;
+}
