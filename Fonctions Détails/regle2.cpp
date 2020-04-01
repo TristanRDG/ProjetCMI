1c710ca8f27e252ccdb7e36c692a3ac6a4a9a8b8
leolcx <61586931+leolcx@users.noreply.github.com>
2020-04-01 17:31:50 +0200

    Update regle2.cpp

--- Fonctions Détails/regle2.cpp
+++ Fonctions Détails/regle2.cpp
@@ -1,32 +1,26 @@
-void regle2() {
-int erreurR2=0;
+bool regle2(int** AdresseTableau, int DefTaille) {
 
-    for (int i=0; i<4; i++)
+    for (int i=0; i<DefTaille; i++)
     {
-        for (int j=0; j<2; j++)
+        for (int j=0; j<DefTaille-2; j++)
         {
-            if (tableauTest[i][j] == tableauTest[i][j+1] && tableauTest[i][j+1] == tableauTest[i][j+2])
+            if (AdresseTableau[i][j] == AdresseTableau[i][j+1] && AdresseTableau[i][j+1] == AdresseTableau[i][j+2])
             {
-                erreurR2++;
+                return false;
             }  
         }
     }
     
-    for (int j=0; j<4; j++)
+    for (int j=0; j<DefTaille; j++)
     {
-        for (int i=0; i<2; i++)
+        for (int i=0; i<DefTaille-2; i++)
         {
-            if (tableauTest[i][j] == tableauTest[i+1][j] && tableauTest[i+1][j] == tableauTest[i+2][j])
+            if (AdresseTableau[i][j] == AdresseTableau[i+1][j] && AdresseTableau[i+1][j] == AdresseTableau[i+2][j])
             {
-                erreurR2++;
+                return false;
             }  
         }
     }
 
-    cout<<"Regle 2 : Pas plus de deux chiffres cote a cote"<<endl;
-   
-    if (erreurR2==0 || erreurR2==1)
-        cout<<"Vous avez fait "<<erreurR2<<" erreur"<<endl;
-    else
-        cout<<"Vous avez fait "<<erreurR2<<" erreurs"<<endl;
+    return true;    
 }
