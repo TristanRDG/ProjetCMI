acb3f0d36c07f103354f995402204ef9d2cc2706
leolcx <61586931+leolcx@users.noreply.github.com>
2020-04-01 17:33:21 +0200

    Update regle3.cpp

--- Fonctions Détails/regle3.cpp
+++ Fonctions Détails/regle3.cpp
@@ -1,164 +1,46 @@
-void regle3 (){
-int resultat(0), erreurR3(0);
+bool regle3 (int** AdresseTableau, int DefTaille){
+    int erreur3 = 0;
 
-    cout<<"Regle 3 : Deux lignes ou deux colonnes ne peuvent pas etre identiques"<<endl;
-	
-	for (int i=0; i<=3; i++)
-    {        
-        resultat = 0;
+    for (int i = 0; i  <= DefTaille-2; i++){           ///Lignes///j < (4*4 - ((i+1)*4))-1///
+        cout<<"i = "<<i<<"\n";
 
-        for (int j=0; j<4; j++)
-        {
-            if (tableauTest[i][j] == tableauTest[i+1][j])
-            {
-            	resultat++;
-            }
-            
-            if (i==0 && resultat==4)
-            {
-                cout<<"Lignes 0 et 1 identiques"<<endl;
-                erreurR3++;
-            }
+        for (int j = i+1; j <= DefTaille-1; j++){
+            cout<<" j = "<<j<<"\n";
 
-            if (i==1 && resultat==4)
-            {
-                cout<<"Lignes 1 et 2 identiques"<<endl;
-                erreurR3++;
-            }
-
-            if (i==2 && resultat==4)
-            {
-                cout<<"Lignes 2 et 3 identiques"<<endl;
-                erreurR3++;
-            }            
-        }
-    }
-
-    for (int i=0; i<=2; i++)
-    {        
-        resultat = 0;
-
-        for (int j=0; j<4; j++)
-        {
-            if (tableauTest[i][j] == tableauTest[i+2][j])
-            {
-            	resultat++;
-            }
-            
-            if (i==0 && resultat==4)
-            {
-                cout<<"Lignes 0 et 2 identiques"<<endl;
-                erreurR3++;
-            }
-
-            if (i==1 && resultat==4)
-            {
-                cout<<"Lignes 1 et 3 identiques"<<endl;
-                erreurR3++;
+            for (int k = 0; k <= DefTaille-1; k++){
+                cout<<"  k = "<<k<<"\n";
+                if (AdresseTableau[i][k] != AdresseTableau[j][k]){
+                    cout<<"BREAK!"<<"\n";
+                    break;
+                }
+                else if (k == 3 && AdresseTableau[i][k] == AdresseTableau[j][k]){
+                    return false;
+                    cout<<"Erreur "<<"\n";
+                }
             }
         }
     }
 
-    for (int i=0; i<=1; i++)
-    {        
-        resultat = 0;
 
-        for (int j=0; j<4; j++)
-        {
-            if (tableauTest[i][j] == tableauTest[i+3][j])
-            {
-            	resultat++;
+        for (int i = 0; i  <= DefTaille-2; i++){           ///colones///
+        cout<<"i = "<<i<<"\n";
+
+        for (int j = i+1; j <= DefTaille-1; j++){
+            cout<<" j = "<<j<<"\n";
+
+            for (int k = 0; k <= DefTaille-1; k++){
+                cout<<"  k = "<<k<<"\n";
+                if (AdresseTableau[k][i] != AdresseTableau[k][j]){
+                    cout<<"BREAK!"<<"\n";
+                    break;
+                }
+                else if (k  == 3 && AdresseTableau[k][i] == AdresseTableau[k][j]){
+                    return false;
+                    cout<<"Erreur "<<"\n";
+                }
             }
-            
-            if (i==0 && resultat==4)
-            {
-                cout<<"Lignes 0 et 3 identiques"<<endl;
-                erreurR3++;
-            }
-            
         }
     }
-
-    for (int j=0; j<=3; j++)
-    {        
-        resultat = 0;
-
-        for (int i=0; i<4; i++)
-        {
-            if (tableauTest[i][j] == tableauTest[i][j+1])
-            {
-            	resultat++;
-            }
             
-            if (j==0 && resultat==4)
-            {
-                cout<<"Colonnes 0 et 1 identiques"<<endl;
-                erreurR3++;
-            }
-
-            if (j==1 && resultat==4)
-            {
-                cout<<"Colonnes 1 et 2 identiques"<<endl;
-                erreurR3++;
-            }
-            
-            if (j==2 && resultat==4)
-            {
-                cout<<"Colonnes 2 et 3 identiques"<<endl;
-                erreurR3++;
-            } 
-            
-        }
-    }
-
-    for (int j=0; j<=2; j++)
-    {        
-        resultat = 0;
-
-        for (int i=0; i<4; i++)
-        {
-            if (tableauTest[i][j] == tableauTest[i][j+2])
-            {
-            	resultat++;
-            }
-            
-            if (j==0 && resultat==4)
-            {
-                cout<<"Colonnes 0 et 2 identiques"<<endl;
-                erreurR3++;
-            }
-
-            if (j==1 && resultat==4)
-            {
-                cout<<"Colonnes 1 et 3 identiques"<<endl;
-                erreurR3++;
-            }
-            
-        }
-    }
-
-    for (int j=0; j<=1; j++)
-    {        
-        resultat = 0;
-
-        for (int i=0; i<4; i++)
-        {
-            if (tableauTest[i][j] == tableauTest[i][j+3])
-            {
-            	resultat++;
-            }
-            
-            if (j==0 && resultat==4)
-            {
-                cout<<"Colonnes 0 et 3 identiques"<<endl;
-                erreurR3++;
-            }   
-        }
-    }
-  
-    if (erreurR3==0 || erreurR3==1)
-        cout<<"Vous avez fait "<<erreurR3<<" erreur"<<endl;
-    else
-        cout<<"Vous avez fait "<<erreurR3<<" erreurs"<<endl;
-
+    return true;
 }
