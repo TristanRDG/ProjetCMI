0eddbcd972bb693fcd853faa75efbeefc7273a8b
leolcx <61586931+leolcx@users.noreply.github.com>
2020-04-04 17:34:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,46 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-#include "Fonctions/regles.cpp" 
-#include "Fonctions/Tableau.cpp"
+
+#include "C:\Users\leo31\Documents\ProjetCMI\regles.cpp"
+#include "C:\Users\leo31\Documents\ProjetCMI\tableau.cpp"
+
 #define EVAL(exp) cout << "Valeur de " << (#exp) <<" : "<< (exp) << endl
 
+int main() {
+
+    cout<<endl;
+    cout<<endl;
+    cout<<"Debut de la partie :"<<endl;
 
 
-int main()
-{
-//      ||VARIABLE GLOBAL||      //
-    int** AdresseTableau;  //Pointeur vers l'adresse du tableaux (premierre valeur) || Pointeur de pointeur !!!
-    int  DefTaille;  //Taille du tableaux 
-//      ||VARIABLE GLOBAL||      //
+    int** AdresseTableau;  // Pointeur vers l'adresse du tableaux (premierre valeur) || Pointeur de pointeur
+    int  DefTaille;  // Taille du tableau
+
 
     tailleTab(&DefTaille);
     AdresseTableau = creaTableaux(DefTaille);
     remplisTableau(AdresseTableau, DefTaille);
 
-    //TEST SI LE TABLEAUX EST JUSTE
+    cout<<endl<<endl;
+    cout<<"Attention ! La premiere ligne/colonne commence a 0, la deuxieme a 1 etc..."<<endl<<endl;
 
-    //EVAL(regle1_1 (AdresseTableau, DefTaille));
-    //EVAL(regle1_2 (AdresseTableau, DefTaille));
-    //EVAL(regle2(AdresseTableau, DefTaille));
-    //EVAL(regle3 (AdresseTableau, DefTaille));
+    while(regle1_1(AdresseTableau, DefTaille)!=true || regle1_2(AdresseTableau, DefTaille)!=true || regle2(AdresseTableau, DefTaille)!=true || regle3(AdresseTableau, DefTaille)!=true)
+    {
+        saisirValeur(AdresseTableau, DefTaille);
+        printTab(AdresseTableau, DefTaille);
+        cout<<endl<<endl;
+    }
+
+    cout<<"VICTOIRE !"<<endl;
     
     //SUPRESION  DU TABLEAUX DE LA MEMOIRE
-
-    for (int k=0; k<=DefTaille; k++){         
-        delete [] AdresseTableau[k];
-    }
-    delete []  AdresseTableau;
+        for (int k=0; k<=DefTaille; k++)
+        {         
+            delete [] AdresseTableau[k];
+        }
+        delete []  AdresseTableau;
+    //////////////////////////////////////
     
-/////////////////////////////////////////////////
-
-    //system("pause");
+    system("pause");
     return 0;
 }
